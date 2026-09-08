b7973c9dfa030c0f24c799c2f4acbce6f2737a00
Tom M <tom@tmewett.com>
2026-09-08 15:51:33 +0100

    tb: simple working

--- timeboxer/main.tcl
+++ timeboxer/main.tcl
@@ -1,24 +1,38 @@
 package require Tk
 
 oo::class create app {
-    variable endTime
+    variable endTime remainingS
     constructor {} {
-        grid [ttk::label .time]
-        set endTime [clock add [clock seconds] 5 seconds]
-        my tick
+        grid [ttk::entry .entry]
+        bind .entry <Key-Return> "[self] set"
+        focus .entry
+        grid [ttk::label .time -font "normal 48" -text "00:00"]
     }
     method tick {} {
-        set remainingTime [expr {$endTime - [clock seconds]}]
-        .time configure -text $remainingTime
-        wm title . "$remainingTime remaining"
-        after 1000 "[self] tick"
+        my update
+        if {$remainingS > 0} {
+            after 1000 "[self] tick"
+        } else {
+            .entry state !disabled
+            .entry selection range 0 end
+            focus .entry
+        }
     }
-    method update {eventsIn} {
-        set events [dict merge {} $eventsIn]
+    method set {} {
+        set endTime [clock add [clock seconds] [.entry get] seconds]
+        my tick
+        .entry state disabled
+        wm iconify .
+    }
+    method update {} {
+        set remainingS [expr {$endTime - [clock seconds]}]
+        puts $remainingS
+        set display "[format %02u [expr {$remainingS / 60}]]:[format %02u [expr {$remainingS % 60}]]"
+        .time configure -text $display
+        wm title . $display
     }
 }
 
 set appO [app new]
-$appO update {}
 
 tkwait window .
