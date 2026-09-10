9f7317ea29921fe0d6764a2fc439a8ea7492f870
Tom M <tom@tmewett.com>
2026-09-10 18:42:25 +0100

    timeboxer mvp!

--- timeboxer/main.tcl
+++ timeboxer/main.tcl
@@ -1,42 +1,59 @@
 package require Tk
 
 oo::class create app {
-    variable endTime remainingS
+    variable endTime
     constructor {} {
+        set endTime false
         grid [ttk::entry .entry]
+        bind . <Key-Escape> "[self] esc"
         bind .entry <Key-Return> "[self] set"
         focus .entry
         grid [ttk::label .time -font "normal 48" -text "00:00"]
     }
     method tick {} {
         my update
-        after 1000 "[self] tick"
-        if {$remainingS > 0} {
-        } else {
-            focus .entry
+        if {$endTime} {
+            after 500 "[self] tick"
         }
     }
+    method esc {} {
+        set endTime false
+        my update
+        focus .entry
+        .entry selection range 0 end
+    }
     method set {} {
-        set endTime [clock add [clock seconds] [.entry get] seconds]
+        set endTime [clock add [clock seconds] [.entry get] minutes]
         my tick
         wm iconify .
     }
     method update {} {
-        set remainingS [expr {$endTime - [clock seconds]}]
-        puts $remainingS
-        if {$remainingS >= 0} {
-            set display "[format %02u [expr {$remainingS / 60}]]:[format %02u [expr {$remainingS % 60}]]"
+        if {$endTime} {
+            set ms [clock milliseconds]
+            set remainingS [expr {$endTime - $ms / 1000}]
+            if {$remainingS >= 0} {
+                set display "[format %02u [expr {$remainingS / 60}]]:[format %02u [expr {$remainingS % 60}]]"
+                set title $display
+            } else {
+                set display "-[format %02u [expr {-$remainingS / 60}]]:[format %02u [expr {-$remainingS % 60}]]"
+                if {$ms % 1000 >= 500} {
+                    set title "Timer done!"
+                } else {
+                    set title $display
+                }
+            }
             .entry state disabled
         } else {
-            set display "-[format %02u [expr {-$remainingS / 60}]]:[format %02u [expr {-$remainingS % 60}]]"
+            set display "00:00"
+            set title "Timeboxer"
             .entry state !disabled
-            .entry selection range 0 end
         }
         .time configure -text $display
-        wm title . $display
+        wm title . $title
     }
 }
 
 set appO [app new]
+$appO update
 
 tkwait window .
